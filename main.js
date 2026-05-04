d9d2a930112966e3a77bd5e762ef0fa84182279e
Dammycodes-all <harliarmeen@gmail.com>
2026-05-04 09:40:12 +0100

    ADD AppImage support and sandbox switch for Electron
    - Enable `no-sandbox` flag when running as AppImage
    - Add `@reforged/maker-appimage` for packaging
    - Update electron-serve usage and config
    - Ignore `/android` in packager ignore list
    - Set module type to `module` in package.json
    - Update dependencies and remove unused ones

--- main.js
+++ main.js
@@ -3,6 +3,10 @@ import serve from "electron-serve";
 
 const loadURL = serve({ directory: "dist" });
 
+if (process.env.APPIMAGE) {
+  app.commandLine.appendSwitch("no-sandbox");
+}
+
 async function createWindow() {
   const win = new BrowserWindow({
     width: 1200,
